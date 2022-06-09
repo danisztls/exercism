b36148c728b6b19ab218012b70063dce69487db9
Daniel Souza <me@posix.dev.br>
2022-06-09 04:31:44 -0300

    feat: rewrite script to download exercises
    The old script isn't working anymore. This works but Exercism is
    allowing only download of unlocked exercises.

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
+# Exercism
 My answers to exercises and problems on [Exercism](https://exercism.io/). Mostly to keep track of my progress.
-
-Script to mass download exercises and save time
-```
-curl -LS https://exercism.io/tracks/rust/exercises | grep "/tracks/rust/exercises/" | awk '{print $3}' | cut -d/ -f5 | cut -d\" -f1 > exercises.txt
-while read in; do exercism download --exercise="$in" --track=rust; done < exercises.txt
-```
---
+++ get-exercises.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+# Download all exercises from a Exercism track and save time
+
+# @author: Daniel Souza <me@posix.dev.br>
+# @license: MIT
+
+# Usage: get-exercises.sh <lang>
+
+# Debug
+# exit when a command has a non-zero exit status
+# note: '||:' at the end of a pipe mask a non-zero exit
+set -e
+
+# exit when referencing undefined variable
+# note: $* and $@ are exceptions
+set -u
+
+# exit if any command in a pipe fail
+set -o pipefail
+
+# Utils
+# decorators
+reset="\e[0m"
+strong="\e[1;39m"
+blue="\e[1;34m"
+# red="\e[1;31m"
+yellow="\e[1;33m"
+green="\e[1;32m"
+
+# Main
+lang="$1"
+
+echo -e "${strong}${yellow}Getting list of exercises\t${reset}\n\n"
+
+exercises=$(
+  curl -LS "https://exercism.io/tracks/${lang}/exercises" |
+  grep -o "tracks/${lang}/exercises/[^&]*" |
+  sed "s/tracks\/${lang}\/exercises\///"
+)
+
+mkdir -p "${lang}"
+pushd "${lang}" &>/dev/null
+
+echo -e "${strong}${yellow}\nDownloading exercises...${reset}\n\n"
+
+while read -r exercise; do
+  echo -e "${strong}\nget... ${blue}${exercise}...${reset}\n"
+  exercism download --exercise="${exercise}" --track="${lang}"
+done <<< "$exercises"
+popd &>/dev/null
+
+echo -e "${strong}${green}All done!${reset}\n"
